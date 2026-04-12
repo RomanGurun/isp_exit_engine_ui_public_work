6fa43f7a03c461e2ea0e47f192383a9fcd9606d8
RomanGurun <romangurung569@gmail.com>
2026-04-12 11:02:05 +0545

    out_screen_page body code added

--- lib/feature/exit_page/screens/out_screen.dart
+++ lib/feature/exit_page/screens/out_screen.dart
@@ -5,8 +5,221 @@ class OutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Welcome Page'),
-    
-    ),);
+    return Scaffold(
+      appBar: AppBar(title: Text('Welcome Page')),
+      body: Center(
+        child: SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.teal),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 11),
+
+            child: Column(
+              children: [
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  padding: EdgeInsets.all(100),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(10),
+                    
+                  ),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  padding: EdgeInsets.all(100),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+                Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Out Screen Button',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+                
+
+
+
+
+
+
+
+                // TextField(controller: TextEditingController(),
+                // enabled: true,
+                // ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'Out Screen Button',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ),
+
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'Out Screen Button',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ),
+
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'Out Screen Button',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ),
+
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'Out Screen Button',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ),
+
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'Out Screen Button',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
