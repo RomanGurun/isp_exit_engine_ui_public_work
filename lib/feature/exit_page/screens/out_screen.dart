ca007c0f8eec817d24884eb8ff24c742f6c6b25d
RomanGurun <romangurung569@gmail.com>
2026-04-23 10:58:10 +0545

    Extra Spacing and Widget tree order fixed

--- lib/feature/exit_page/screens/out_screen.dart
+++ lib/feature/exit_page/screens/out_screen.dart
@@ -6,7 +6,7 @@ import 'package:isp_exit_form_implementation/feature/exit_page/screens/exit_page
 class OutScreen extends StatelessWidget {
   OutScreen({super.key});
 
-  final List items = [
+  final List<String> items = [
     "pen",
     "laptop",
     "pencil",
@@ -20,7 +20,7 @@ class OutScreen extends StatelessWidget {
     'chair',
     'table',
   ];
-  final List icon = [
+  final List<IconData> icon = [
     Icons.mobile_off_outlined,
     Icons.bike_scooter,
     Icons.house,
@@ -94,20 +94,23 @@ class OutScreen extends StatelessWidget {
       //     },
       // ),
       body: Container(
-        margin: EdgeInsets.only(top:30,left: 10,right: 10,bottom: 10),
-        padding: EdgeInsets.all(20),
-        height: 600,
+        margin: EdgeInsets.only(top:10,left:10,right:10,bottom:10),
+       padding: EdgeInsets.all(10),
         decoration: BoxDecoration(
-          color: ExitColors.pureWhite,
-
-          borderRadius: BorderRadius.circular(30),
-          border: Border.all(color: ExitColors.g8, width: 3.3),
-        ),
+       
+        borderRadius:   BorderRadius.circular(10),
+        border:Border.all(
+          color: const Color.fromRGBO(59, 109, 17, 1),
+          width: 3,
+        )
+          ),
         child: GridView.builder(
           itemCount: icon.length,
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 3,
-
+        
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
             childAspectRatio: 1,
@@ -122,24 +125,23 @@ class OutScreen extends StatelessWidget {
                 elevation: 4,
                 // margin: EdgeInsets.only(top:10),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Center(
-                      child: Container(
-                        
-                        margin: EdgeInsets.only(top: 30),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: ExitColors.g8, width: 2.5),
-color: ExitColors.pureWhite,
-                        ),
-                        child: Icon(
-                          icon[index],
-                          size: 30,
-                          color: ExitColors.pureBlack,
-                        ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: ExitColors.g8, width: 2.5),
+                            color: ExitColors.pureWhite,
+                      ),
+                      child: Icon(
+                        icon[index],
+                        size: 30,
+                        color: ExitColors.pureBlack,
+        
                       ),
                     ),
-                    SizedBox(height: 10,),
+                  const SizedBox(height: 10,),
                     Text(
                       "${items[index]}",
                       style: TextStyle(
